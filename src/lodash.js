1f5551269d82e5bcbfc5b64d32e9febad5f85e65
Ryan McNair <ryan.mcnair@gmail.com>
2021-07-01 16:18:37 -0500

    completed immutable section

---
+++ src/lodash.js
@@ -0,0 +1,12 @@
+import { compose, pipe } from "lodash/fp";
+
+let input = "   JavaScript   ";
+
+const trim = str => str.trim();
+const wrap = type => str => `<${type}>${str}</${type}>`;
+const toLowerCase = str => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap("div"));
+transform(input);
+
+console.log(transform(input));
